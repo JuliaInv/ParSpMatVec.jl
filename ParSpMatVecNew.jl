6e2b06406439b7ea09612505c17de97533e73ce9
Eran Treister <erantreister@gmail.com>
2016-03-04 11:00:46 -0800

    delete misplaced files

--- ParSpMatVecNew.jl
+++
@@ -1,9 +0,0 @@
-module ParSpMatVec
-
-const spmatveclib  = abspath(joinpath(splitdir(Base.source_path())[1],"..","deps","builds","ParSpMatVec"))
-
-include("A_mul_B.jl")
-include("Ac_mul_B.jl")
-
-end  # module ParSpMatVec
-
