60db9d04cfa609731e64a844e5bb22c5f06b179f
erantreister <erantreister@gmail.com>
2019-03-15 16:40:32 +0200

    Updating to support package building info

--- src/ParSpMatVec.jl
+++ src/ParSpMatVec.jl
@@ -1,6 +1,7 @@
 module ParSpMatVec
 using SparseArrays
 using LinearAlgebra
+using Libdl
 const spmatveclib  = abspath(joinpath(splitdir(Base.source_path())[1],"..","deps","builds","ParSpMatVec"))
 
 include("A_mul_B.jl")
@@ -8,16 +9,18 @@ include("Ac_mul_B.jl")
 
 export isBuilt
 function isBuilt()
-	A = sprandn(10,10,0.25);
-	x = rand(10);
-	y = rand(10);
-	try 
-		ParSpMatVec.Ac_mul_B!( 1.0, A, x, 0.0, y, 1);
-		return true; 
-	catch
-		warn("ParSpMatVec is not built");
-		return false;
-	end
+	return find_library(["ParSpMatVec"])!=""
+	# A = sprandn(10,10,0.25);
+	# x = rand(10);
+	# y = rand(10);
+	# println("IM HERE");
+	# try 
+		# ParSpMatVec.Ac_mul_B!( 1.0, A, x, 0.0, y, 1);
+		# return true; 
+	# catch
+		# warn("ParSpMatVec is not built");
+		# return false;
+	# end
 end
 
 
