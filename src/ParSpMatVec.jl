6bbdb78f652a2c0267cb2490c7b327d9e02c3576
erantreister <erantreister@gmail.com>
2019-03-15 14:33:21 +0200

    Adding isBuilt()

--- src/ParSpMatVec.jl
+++ src/ParSpMatVec.jl
@@ -6,4 +6,19 @@ const spmatveclib  = abspath(joinpath(splitdir(Base.source_path())[1],"..","deps
 include("A_mul_B.jl")
 include("Ac_mul_B.jl")
 
+export isBuilt
+function isBuilt()
+	A = sprandn(10,10,0.25);
+	x = rand(10);
+	y = rand(10);
+	try 
+		ParSpMatVec.Ac_mul_B!( 1.0, A, x, 0.0, y, 1);
+		return true; 
+	catch
+		warn("ParSpMatVec is not built");
+		return false;
+	end
+end
+
+
 end # module
